3ab0f98360af0397664d465f905d5096532e7155
E.Z. Hart <hartez@gmail.com>
2012-12-09 13:40:57 -0700

    Handling copy error message on module import

--- todo.psm1
+++ todo.psm1
@@ -1,8 +1,6 @@
 $assemblyPath = ($PSScriptRoot + '\staging\todotxtlib.net.dll')
 $assemblyLoadPath = ($PSScriptRoot + '\lib')
 
-Write-Host $assemblyLoadPath
-
 if(!(Test-Path $assemblyLoadPath))
 {
 	New-Item $assemblyLoadPath -ItemType directory
@@ -10,7 +8,17 @@ if(!(Test-Path $assemblyLoadPath))
 
 $assemblyLoadPath = $assemblyLoadPath + '\todotxtlib.net.dll'
 
-Copy-Item -Path $assemblyPath -Destination $assemblyLoadPath
+# Before we try to load up the newest version of the DLL, we need to see if it's already loaded
+# so we'll try to New-Object a task list; if it fails, we'll know it's safe to copy the dll
+
+Try
+{
+	Copy-Item -Path $assemblyPath -Destination $assemblyLoadPath	
+}
+Catch
+{
+	[system.exception]
+}
 
 Add-Type -Path $assemblyLoadPath
 
