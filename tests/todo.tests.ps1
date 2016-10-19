795785d1b1852d1f88858647ca69cbc6c482ec36
E.Z. Hart <hartez@gmail.com>
2016-10-18 20:56:37 -0600

    Setting up solution

---
+++ tests/todo.tests.ps1
@@ -0,0 +1,15 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-HelloWorld" {
+    It "does nothing because there's no input file" {
+        Get-HelloWorld | Should BeNullOrEmpty
+    }
+
+    It "outputs the last line of the input file" {
+        Get-HelloWorld data.txt | Should Be 'This is the last line'
+    }
+}
+
+
