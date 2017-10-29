b413af9f7fdf048ede116011e19963a4a38701f1
E.Z. Hart <hartez@gmail.com>
2017-10-29 17:47:06 -0600

    Add prepending to Edit-Task

--- Edit-Task/Edit-Task.psm1
+++ Edit-Task/Edit-Task.psm1
@@ -8,13 +8,20 @@ function Edit-Task {
 	param(
 		[Parameter(Mandatory=$true)]
 		[int] $index,
-		[string] $append
+		[string] $append,
+		[string] $prepend
 	)
 
 	if($append) { 
 		AppendToTask -Index $index -Append $append
 	}
+
+	if($prepend) { 
+		PrependToTask -Index $index -Prepend $prepend
+	}
 }
+
+## TODO Refactor these, everything is pretty much the same except which method on TaskList is called
 
 function AppendToTask {
 	param(
@@ -31,6 +38,29 @@ function AppendToTask {
 			$list.AppendToTask($index, $append)
 			$list.ToOutput() | Set-Content $TODO_FILE
 			
+			if($TODOTXT_VERBOSE)
+			{
+				Write-Host ("$index " + $list[$index-1].Body)
+			}
+		}
+	}
+}
+
+function PrependToTask {
+	param(
+		[int] $index,
+		[string] $prepend
+	)
+
+	$list = Get-TaskList
+	
+	if($prepend)
+	{
+		if($index -le $list.Count)
+		{
+			$list.PrependToTask($index, $prepend)
+			$list.ToOutput() | Set-Content $TODO_FILE
+		
 			if($TODOTXT_VERBOSE)
 			{
 				Write-Host ("$index " + $list[$index-1].Body)
--- tests/Edit-Task.tests.ps1
+++ tests/Edit-Task.tests.ps1
@@ -1,20 +1,26 @@
 ﻿. (Join-Path -Path ($MyInvocation.MyCommand.Path | Split-Path) -ChildPath "TestSetup.ps1")
 EnsureTestEnvironment($MyInvocation.MyCommand.Path)
 
+function SetupTempList {
+	param([string] $path)
+
+	Set-Variable -Name TODO_FILE -Value $path -Scope Global
+
+	$content = @"
+This is the first line
+This is the second line
+This is the last line
+"@
+	Set-Content -Path $TODO_FILE -Value $content
+}
+
 Describe "Edit-Task" {
 	
 	Context "append tests" {
 		
 		BeforeEach {
 			# Set up a throwaway txt data file
-			Set-Variable -Name TODO_FILE -Value ".\tests\temp\appendtests.txt" -Scope Global
-
-			$content = @"
-This is the first line
-This is the second line
-This is the last line
-"@
-			Set-Content -Path $TODO_FILE -Value $content
+			SetupTempList -Path ".\tests\temp\appendtests.txt"
 		}
 
 		AfterEach {
@@ -27,10 +33,35 @@ This is the last line
 			Get-Task second | Should Be "This is the second line; this is appended"
 		}
 
-		It "should append content to the second task using the Index parameter " {
+		It "should append content to the second task using the Index parameter" {
 			Edit-Task -Index 2 -Append "; this is appended"
 			Get-Task second | Should Be "This is the second line; this is appended"
 		}
 	}
 
+	Context "prepend tests" {
+		
+		BeforeEach {
+			# Set up a throwaway txt data file
+			SetupTempList -Path ".\tests\temp\prependtests.txt"
+		}
+
+		AfterEach {
+			Remove-Item $TODO_FILE
+			Remove-Variable -Name TODO_FILE -Scope Global
+		}
+		
+		It "should prepend content to the second task (index implied)" {
+			Edit-Task 2 -Prepend "this is in front;"
+			Get-Task second | Should Be "this is in front;This is the second line"
+		}
+
+		It "should prepend content to the second task using the Index parameter" {
+			Edit-Task -Index 2 -Prepend "this is in front;"
+			Get-Task second | Should Be "this is in front;This is the second line"
+		}
+	}
+
+	## TODO Add tests where Edit-Task takes a path to each context. (They'll fail, right now it assumes $TODO_FILE)
 }
+
